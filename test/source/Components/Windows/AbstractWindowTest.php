b4327c109cff62e237460009b2eb8463641774b5
Stefan Löwe <stefan@loewe.ws>
2010-12-22 07:46:48 +0000

    wip-commit

--- test/source/Components/Windows/AbstractWindowTest.php
+++ test/source/Components/Windows/AbstractWindowTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Woody\Components\Windows;
 
-use Woody\Utils\Misc\Rectangle;
+use Woody\Utils\Geom\Point;
+use Woody\Utils\Geom\Dimension;
+use Woody\System\System;
 
 
 //require_once dirname(__FILE__).'/../../../../source/Components/Windows/AbstractWindow.class.inc';
@@ -23,9 +25,9 @@ class AbstractWindowTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $this->object = new MainWindow('myWin2', 'MyWin2', new Rectangle(50, 50, 300, 200));
+        $this->object = new MainWindow('myWin2', 'MyWin2', new Point(50, 50), new Dimension(300, 200));
+
         $this->object->create();
-        $this->object->initialize();
     }
 
     /**
@@ -34,98 +36,83 @@ class AbstractWindowTest extends \PHPUnit_Framework_TestCase
      */
     protected function tearDown()
     {
-        //$this->object->destroy();
-    }
-
-    /**
-     * @todo Implement testDestroy().
-     */
-    public function testDestroy()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testCreate().
-     */
-    public function testCreate()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @todo Implement testInitialize().
-     */
-    public function testInitialize()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $this->object->destroy();
     }
 
     public function testMoveBy()
     {
         $this->object->moveBy(10, 10);
-        $this->assertEquals(array(60, 60), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(60, 60)));
+
         $this->object->moveBy(100, 300);
-        $this->assertEquals(array(160, 360), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(160, 360)));
+
         $this->object->moveBy(-300, 300);
-        $this->assertEquals(array(-140, 660), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(System::WIN_MIN_LEFT, 660)));
+
         $this->object->moveBy(140, -660);
-        $this->assertEquals(array(0, 0), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(140, System::WIN_MIN_TOP)));
+
         $this->object->moveBy(300, 200);
-        $this->assertEquals(array(300, 200), $this->object->getPosition());usleep(50000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(440, 200)));
     }
 
     public function testMoveTo()
     {
         $this->object->moveTo(0, 0);
-        $this->assertEquals(array(0, 0), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(System::WIN_MIN_LEFT, System::WIN_MIN_TOP)));
+
         $this->object->moveTo(400, 300);
-        $this->assertEquals(array(400, 300), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(400, 300)));
+
         $this->object->moveTo(4000, 3000);
-        $this->assertEquals(array(4000, 3000), $this->object->getPosition());
+        $this->assertTrue($this->object->getPosition()->equals(new Point(4000, 3000)));
 
         $this->object->moveTo(-400, -300);
-        $this->assertEquals(array(-400, -300), $this->object->getPosition());
-usleep(100000);
+        $this->assertTrue($this->object->getPosition()->equals(new Point(System::WIN_MIN_LEFT, System::WIN_MIN_TOP)));
+
         $this->object->moveTo(0, 0);
-        $this->assertEquals(array(0, 0), $this->object->getPosition());
+        $this->assertTrue($this->object->getPosition()->equals(new Point(System::WIN_MIN_LEFT, System::WIN_MIN_TOP)));
     }
 
-    /**
-     * @todo Implement testResizeBy().
-     */
     public function testResizeBy()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+//usleep(1000000);
+        $this->object->resizeBy(0, 0);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(300, 200)));
+
+        $this->object->resizeBy(100, 200);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(400, 400)));
+
+        $this->object->resizeBy(500, 600);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(900, 1000)));
+
+        $this->object->resizeBy(-878, -966);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(System::WIN_MIN_WIDTH, System::WIN_MIN_HEIGHT)));
+
+        $this->object->resizeBy(-200, -100);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(System::WIN_MIN_WIDTH, System::WIN_MIN_HEIGHT)));
+
+        $this->object->resizeBy(177, 166);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(300, 200)));
     }
 
-    /**
-     * @todo Implement testResizeTo().
-     */
     public function testResizeTo()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-                'This test has not been implemented yet.'
-        );
+        $this->object->resizeTo(300, 200);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(300, 200)));
+
+        $this->object->resizeTo(600, 400);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(600, 400)));
+
+        $this->object->resizeTo(0, 0);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(System::WIN_MIN_WIDTH, System::WIN_MIN_HEIGHT)));
+
+        $this->object->resizeTo(-100, -200);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(System::WIN_MIN_WIDTH, System::WIN_MIN_HEIGHT)));
+
+        $this->object->resizeTo(300, 200);
+        $this->assertTrue($this->object->getDimension()->equals(new Dimension(300, 200)));
     }
 }
 ?>
