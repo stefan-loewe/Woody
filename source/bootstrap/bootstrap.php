2eb4c09d2238e1d27f93a7b4afd2ec2ecc63cda3
Stefan Löwe <stefan@loewe.ws>
2011-11-27 20:36:57 +0000

    removed '.class' token from all files, to have a unique naming convention, corresponding to other projects

--- source/bootstrap/bootstrap.php
+++ source/bootstrap/bootstrap.php
@@ -4,7 +4,7 @@ define('INSTALLATION_FOLDER', str_replace('\\', '/', realpath(__DIR__.'/../..'))
 define('SOURCE_FOLDER', INSTALLATION_FOLDER.'/source');
 
 require_once INSTALLATION_FOLDER.'/lib/winbinder.php';
-require_once SOURCE_FOLDER.'/Utils/Autoload/Autoloader.class.inc';
+require_once SOURCE_FOLDER.'/Utils/Autoload/Autoloader.inc';
 
 $autoloader = new \Woody\Utils\Autoload\Autoloader(SOURCE_FOLDER.'/');
 
--- source/index.php
+++ source/index.php
@@ -29,65 +29,14 @@ var_dump(phpversion());
 
 $win = new MainWindow('MyWin2', new Point(50, 50), new Dimension(800, 600));
 $win->create();
-//$box1 = new EditBox("abc", new Point(20, 20), new Dimension(720, 18));
-//$win->add($box1);
 
+$server = new \Woody\Utils\Sockets\HTMLControlServer(8008);
 
+$timer = new Timer(function() use ($server){}
+    , $win, 1000);
 
-//$htmlControl = new Woody\Components\Controls\HTMLControl('http://localhost:8008?id=3', new Point(50, 50), new Dimension(600, 400));
-//$win->add($htmlControl);
-//$htmlControl->setUrl("www.wieistmeineip.de");
-
-$calendar = new Woody\Components\Controls\Calendar(new Point(50, 50), new Dimension(300, 400));
-
-$win->add($calendar);
-
-$date = DateTime::createFromFormat('Y-m-d H:i:s', "2012-03-28 00:00:00");
-var_dump('1. $date '.$date->format('Y-m-d H:i:s'));
-$calendar->setDate($date);
-var_dump('2. $date '.$calendar->getDate()->format('Y-m-d H:i:s'));
-$win->startEventHandler();
-/*
-$win = new MainWindow('MyWin2', new Point(50, 50), new Dimension(800, 600));
-
-$win->create(null);
-
-$frame = new \Woody\Components\Controls\Frame('my new frame', new Point(20, 20), new Dimension(760, 200));
-$win->add($frame);
-
-$box1 = new EditBox("1111", new Point(20, 20), new Dimension(720, 18));
-$box2 = new EditBox("2222", new Point(20, 240), new Dimension(760, 18));
-
-$frame->add($box1);
-$win->add($box2);
-
-$win->add($t = new Woody\Components\Controls\Tab("noneReally", new Point(20, 280), new Dimension(760, 200)));
-$t->addPage();
-
-$frame2 = new \Woody\Components\Controls\Frame('my new frame', new Point(40, 320), new Dimension(720, 160));
-$win->add($frame2);
-
-$frame2->add(new EditBox("3333", new Point(40, 20), new Dimension(720, 18)));
-
-$timerTest = new Timer(function() use ($box1, $timerTest)
-                   {
-                            $box1->setValue('abc');
-
-                            $timerTest->destroy();
-                        }, 500);
-$timerTest->start($win);
-
-$timerTest2 = new Timer(null, 750);
-
-$stdClass = new stdClass();
-$stdClass->cnt = 0;
-$timerTest2->setCallback($f = function() use ($box1, $timerTest2, $stdClass)
-                   {var_dump($stdClass->cnt);
-                            $box1->setValue('def'.($stdClass->cnt++));
-
-                            //$timerTest2->destroy();
-                        });
-$timerTest2->start($win);
+$htmlControl = new Woody\Components\Controls\HTMLControl('http://localhost:8008?id=3', new Point(50, 50), new Dimension(600, 400));
+$win->add($htmlControl);
+$htmlControl->setUrl("www.wieistmeineip.de");
 
 $win->startEventHandler();
-*/
