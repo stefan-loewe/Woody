61e889bff3ab5fa6bf291ab012a29f808010e790
Stefan Löwe <stefan@loewe.ws>
2012-02-22 20:06:53 +0000

    tried to incorporate example that prints contents of local file to html control, via HtmlControlServer - while working with browsers, output to  html control fails, when content is longer than around 1000 bytes

--- doc/examples/htmlControlWithLocalFiles.php
+++ doc/examples/htmlControlWithLocalFiles.php
@@ -19,28 +19,50 @@ use \Utils\Sockets\ServerSocket;
 require_once(realpath(__DIR__.'../../../source/bootstrap/bootstrap.php'));
 
 
-$win = new MainWindow('MyWin2', new Point(50, 50), new Dimension(800, 600));
+$win = new MainWindow('MyWin2', new Point(50, 50), new Dimension(800, 500));
 $win->create();
 
-if(TRUE) {
+if(!TRUE) {
     $server = new \Woody\Server\BuiltInWebServer($win, 8008);
     $server->run();
 } else {
     $server = new \Woody\Server\HtmlControlServer($win, 8008);
-    $server->run(100);
+    $server->run(1000);
 }
 
-$htmlControl = new Woody\Components\Controls\HTMLControl('http://localhost:8008?id=3', new Point(50, 50), new Dimension(600, 400));
+$htmlControl = new Woody\Components\Controls\HTMLControl('http://localhost:8008', new Point(50, 50), new Dimension(600, 300));
+$server->register($htmlControl);
 $win->add($htmlControl);
 
-$bntShowFileDialog = new \Woody\Components\Controls\PushButton("browse ...", new Point(50, 500), new Dimension(100, 22));
-$bntShowFileDialog->addActionListener(new \Woody\Event\ActionAdapter(function() use ($win) {
-                                                                        $file = trim(wb_sys_dlg_open($win->getControlID(), 'select file ...'));
-                                                                        var_dump($file);
-                                                                        $contents = file_get_contents($file);
-                                                                        file_put_contents('D:\workspace\programming\PHP\woody\source\buffer.html', $contents);
-                                                                    }));
+$file = new stdClass();
+$file->name = null;
+
+$cb = function($event) use ($win, $file) {
+        $contents = time();
+        if($file->name != null)
+            $contents = file_get_contents ($file->name);
+
+        $event->type->write('<html><body><a href=\'javascript:alert(navigator.appName + "\n" + "Browser Version: " + navigator.appVersion);\'>debug</a><a href=".">ping</a><br>'.$contents.'</body></html>');
+    };
+$htmlControl->addActionListener(new \Woody\Event\ActionAdapter($cb));
+
+$bntShowFileDialog = new \Woody\Components\Controls\PushButton("browse ...", new Point(50, 370), new Dimension(100, 22));
+$bntShowFileDialog->addActionListener(new \Woody\Event\ActionAdapter(function() use ($win, $file) {
+    $file->name = trim(wb_sys_dlg_open($win->getControlID(), 'select file ...'));
+}));
 $win->add($bntShowFileDialog);
+
+$bntRefresh = new \Woody\Components\Controls\PushButton("refresh", new Point(170, 370), new Dimension(100, 22));
+$bntRefresh->addActionListener(new \Woody\Event\ActionAdapter(function() use ($htmlControl) {
+    $htmlControl->setUrl('http://127.0.0.1:8008/?id='.time());
+}));
+$win->add($bntRefresh);
+
+$bntApi = new \Woody\Components\Controls\PushButton("refresh", new Point(270, 370), new Dimension(100, 22));
+$bntApi->addActionListener(new \Woody\Event\ActionAdapter(function() use ($htmlControl) {
+    $htmlControl->setUrl('http://127.0.0.1/woody/doc/api/');
+}));
+$win->add($bntApi);
 /*
     Woody\Event\EventHandler::addEventHandler($htmlControl2,
     function($control, ServerSocket $clientSocket, HttpRequest $httpRequest)
