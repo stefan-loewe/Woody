58ce5617bb47545d4f2650a3acb7d4253193a6ed
Stefan Löwe <stefan@loewe.ws>
2012-09-27 09:43:48 +0200

    fixed include paths after moving bootstrap.php

--- source/bootstrap.php
+++ source/bootstrap.php
@@ -6,7 +6,7 @@ use \Woody\Components\Component;
 
 error_reporting(E_ALL | E_STRICT);
 
-define('INSTALLATION_FOLDER', str_replace('\\', '/', realpath(__DIR__.'/../..')));
+define('INSTALLATION_FOLDER', str_replace('\\', '/', realpath(__DIR__.'/..')));
 define('SOURCE_FOLDER', INSTALLATION_FOLDER.'/source');
 
 require_once INSTALLATION_FOLDER.'/lib/winbinder.php';
