23793838cb00b813a13ea56fc146cbd515fc0a8d
Stefan Löwe <loewe@fim.uni-passau.de>
2014-09-13 01:55:23 +0200

    added methods for selection to the ListBoxMulti class

--- source/ws/loewe/Woody/Components/Controls/ListBoxMulti.php
+++ source/ws/loewe/Woody/Components/Controls/ListBoxMulti.php
@@ -6,11 +6,11 @@ use \ws\loewe\Utils\Geom\Point;
 use \ws\loewe\Utils\Geom\Dimension;
 
 class ListBoxMulti extends ListBox {
-  
+
   private static $LB_GETSELCOUNT = 0x190;
-  
+
   private static $LB_GETSELITEMS = 0x191;
-  
+
   /**
    * This method acts as the constructor of the class.
    *
@@ -24,6 +24,11 @@ class ListBoxMulti extends ListBox {
     $this->style  = $this->style | WBC_MULTISELECT;
   }
 
+  /**
+   * This method gets the currently selected elements.
+   *
+   * @return mixed[]
+   */
   public function getSelectedElements() {
     $values = new \ArrayObject();
 
@@ -46,7 +51,26 @@ class ListBoxMulti extends ListBox {
     return $values;
   }
 
+  /**
+   * This method determines the number of selected elements.
+   *
+   * @return int
+   */
   public function getSelectionCount() {
     return wb_send_message($this->controlID, self::$LB_GETSELCOUNT, null, null);
+  }
+
+  /**
+   * This method sets a selected element. The previously selected elements stay selected.
+   *
+   * @param mixed $element the element to set selected.
+   * @return ListBoxMulti $this
+   */
+  public function setSelectedElement($element) {
+    if($this->model->getIndexOf($element) !== -1) {
+      wb_send_message($this->controlID, 0x0185, 1, $this->model->getIndexOf($element));
+    }
+
+    return $this;
   }
 }
